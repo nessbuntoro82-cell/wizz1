dc8082aefdba82f495892145ddbbc70034c28d23
nessbuntoro82-cell <nessbuntoro82@gmail.com>
2026-03-13 20:35:41 +0700

    Refactor script loading mechanism in kaitun.lua

--- kaitun.lua
+++ kaitun.lua
@@ -163,5 +163,3 @@ _G.FishItConfig = _G.FishItConfig or {
 script_key="472615EE4C94A86AFA1AF45C1AB477B7";
 
 local s,r repeat s,r=pcall(function()return game:HttpGet("https://raw.githubusercontent.com/FnDXueyi/roblog/refs/heads/main/fishit-78c86024ea87c8eca577549807421962.lua")end)wait(1)until s;loadstring(r)()
-
-loadstring(game:HttpGet("https://raw.githubusercontent.com/FnDXueyi/list/refs/heads/main/game"))()
