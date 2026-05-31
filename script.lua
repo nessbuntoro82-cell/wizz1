a5d120f9be01eaf3926f5c9a414c7cf15434a7ce
nessbuntoro82-cell <nessbuntoro82@gmail.com>
2026-05-31 17:37:55 +0700

    Update script.lua

--- script.lua
+++ script.lua
@@ -93,7 +93,7 @@ _G.FishItConfig = _G.FishItConfig or {
         },
     },
     ["Event"] = {
-        ["Start Farm"] = false,
+        ["Start Farm"] = true,
         ["Minimum Rod"] = "Ghostfinn Rod",
         ["Event List"] = {
             "Megalodon Hunt",
