4bb79bb6ab2fa3d911ac08150f76ab3063839953
nessbuntoro82-cell <nessbuntoro82@gmail.com>
2026-06-20 00:33:13 +0700

    Update script.lua

--- script.lua
+++ script.lua
@@ -11,7 +11,7 @@ _G.FishItConfig = _G.FishItConfig or {
             -- {Variant = "Leviathan Rage"}, -- Variant Only
             -- {Tier = "Secret", Variant = "Leviathan Rage"},  -- Tier + Variant
         },
-        ["Trade Timeout"] = 30, 
+        ["Trade Timeout"] = 50, 
         ["Auto Accept Trade"] = true,
         ["Auto Friend Request"] = true,
     },
