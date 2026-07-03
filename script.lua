36d2052905e096c453d5b6a1aaf6619685382b29
nessbuntoro82-cell <nessbuntoro82@gmail.com>
2026-07-03 13:01:45 +0700

    Update script.lua

--- script.lua
+++ script.lua
@@ -112,7 +112,7 @@ _G.FishItConfig = _G.FishItConfig or {
         },
     },
     ["Enchant"] = {
-        ["Auto Enchant"] = true,
+        ["Auto Enchant"] = false,
         ["Roll Enchant"] = false,
         ["Evolved Roll Enchant"] = true,
         ["Farm Until Enchant"] = false,
