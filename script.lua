a68b88b246cbb72a4f2e6cfa9a2cdf0cc2f7152f
nessbuntoro82-cell <nessbuntoro82@gmail.com>
2026-07-20 08:47:12 +0700

    Remove 'Reeler II' from Enchant List
    Removed 'Reeler II' from the enchant list.

--- script.lua
+++ script.lua
@@ -116,7 +116,6 @@ _G.FishItConfig = _G.FishItConfig or {
         ["Enchant After X Stone"] = 20,
         ["Enchant List"] = {
             "SECRET Hunter",
-            "Reeler II",
         },
         ["Second Enchant"] = true,
         ["Allowed Sacrifice"] = {
