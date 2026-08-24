8b3e2a4f9057f1158aac544c8f12ddcdcb93daee
nessbuntoro82-cell <nessbuntoro82@gmail.com>
2026-08-24 14:08:05 +0700

    Update script.lua

--- script.lua
+++ script.lua
@@ -105,7 +105,7 @@ _G.FishItConfig = _G.FishItConfig or {
     },
     ["Event"] = {
         ["Start Farm"] = true,
-        ["Minimum Rod"] = "Element Rod",
+        ["Minimum Rod"] = "Element Rod","Ghostfinn Rod",
         ["Event List"] = {
             "Mutant Runic Koi",
             "Elemental Event",
