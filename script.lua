c38e8d3abcce9aea262a836fdaa767cd8ab6205b
nessbuntoro82-cell <nessbuntoro82@gmail.com>
2026-08-30 19:58:15 +0700

    Update script.lua

--- script.lua
+++ script.lua
@@ -163,8 +163,8 @@ _G.FishItConfig = _G.FishItConfig or {
     },
     
     ["FPS Cap"] = 0,
-    ["ExtremeFpsBoost"] = false,
-    ["UltimatePerformance"] = true,
+    ["ExtremeFpsBoost"] = true,
+    ["UltimatePerformance"] = false,
     ["Disable3DRender"] = true,
     ["AutoRemovePlayer"] = true,
 
