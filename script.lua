0c3b951e26a945be70d359b98d2f70f187629abe
nessbuntoro82-cell <nessbuntoro82@gmail.com>
2026-08-31 10:57:53 +0700

    Update script.lua

--- script.lua
+++ script.lua
@@ -17,7 +17,7 @@ _G.FishItConfig = _G.FishItConfig or {
     },
     ["Auto Trade"] = {
         ["Enabled"] = true,
-        ["Whitelist Username"] = {"wizzxness2","wizzxness1"},
+        ["Whitelist Username"] = {"wizzxness2","wizzxness1","wizzxness3"},
         ["Category Fish"] = {
             "Secret",
             -- {Tier = "Mythic", Variant = "Stone"}, -- Tier + Variant
